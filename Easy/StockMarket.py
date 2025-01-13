c025c97e18de6e6f22b634368688f21bc2822eb1
alorenzomeza@gmail.com <alorenzomeza@gmail.com>
2025-01-13 11:27:32 -0600

    added comments

--- Easy/StockMarket.py
+++ Easy/StockMarket.py
@@ -2,11 +2,16 @@ class StockMarket:
     def maxProfit(self, prices):
         l, r = 0, 1
         maxP = 0
-
+        
+        #loop to compare prices
         while r < len(prices):
+            #if left price is lt right price loop
             if prices[l] < prices[r]:
+                #profit equals right price minus left
                 profit = prices[r] - prices[l]
+                #compare maxP vs profit and change max to the higher number
                 maxP = max(maxP, profit)
+            #if l=r shift increment and check
             else:
                 l = r
             r += 1
