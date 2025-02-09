c97fa1cefeeb46ac78a609e814b0be69907cd560
alorenzomeza@gmail.com <alorenzomeza@gmail.com>
2025-02-08 22:39:46 -0600

    added concise comments

--- Medium/MaxArea.py
+++ Medium/MaxArea.py
@@ -1,17 +1,29 @@
+"""
+This solution takes the height of each object
+and calculates the max area between them.
+
+obj1=MaxArea()
+obj1.bucketMaxArea(arr1)
+
+"""
+
+
 class MaxArea:
-    def bucketMaxArea(self, heights):
-        #dec's left and right pointer
+    def bucketMaxArea(heights):
+        #declarations of left and right pointers
         l, r = 0, len(heights) - 1
         res = 0
-
+        
+        #while left side is < right we will calculate
         while l < r:
-            #calculate area then replace res if less than area
+            #calculate area then replace res with higher number
             area = min(heights[l], heights[r]) * (r - l)
             res = max(res, area)
+            
             #increment if left is lt or eq to r side
             if heights[l] <= heights[r]:
                 l += 1
-            #else de-inc right side
+            #else de-inc right side and recalculate
             else:
                 r -= 1
         return res
